45ea542688ca82fac6d204db20b5a2ccd11b6842
Ishou <colin.auberger@gmail.com>
2026-05-30 17:41:43 +0200

    chore(identity-domain): apply spotless to Role
    Signed-off-by: Ishou <colin.auberger@gmail.com>

--- identity/domain/src/main/kotlin/com/bliss/identity/domain/user/Role.kt
+++ identity/domain/src/main/kotlin/com/bliss/identity/domain/user/Role.kt
@@ -1,7 +1,9 @@
 package com.bliss.identity.domain.user
 
 /** Authz primitive (ADR-0060). `wire` is the stable DB + event-payload spelling. */
-enum class Role(val wire: String) {
+enum class Role(
+    val wire: String,
+) {
     PLAYER("player"),
     MAINTAINER("maintainer"),
     ;
